07b516c416cc5e8656c3892519cb12db0dbf5578
Norman Davie <ecompservices@gmail.com>
2024-09-01 21:46:48 -0300

    Formatting completed

--- snoop_server.py
+++ snoop_server.py
@@ -16,9 +16,9 @@ if local_ip[0:3] == "127":
     local_ip = socket.gethostbyname(hostname) 
 
 Spoofed_AppleWin_hostname  	= 'localhost'
-Spoofed_AppleWin_port 		= 1986
+Spoofed_AppleWin_port 		= 1985
 
-Real_AppleWin_hostname 		= 'ARIEL-AMD7'
+Real_AppleWin_hostname 		= 'fujinet-vm.local'
 Real_AppleWin_port			= 1985  
 
 run  = [1]
@@ -42,22 +42,22 @@ real applewin -> spoofed applewin [ display hex] -> fujinet
 
 def threaded_forward(in_connection, out_connection, in_msg, out_msg, state):
 
-    print(f"Started forward {in_msg} -> {out_msg}\n")
+    print(f"Forward messages from {in_msg.strip()} -> {out_msg.strip()}")
     while state == run :
         # get message from client
         try:
             slip_message = in_connection.recv(packet_size)
-            print(slip_message)
             if slip_message != b'':
-                print(f"from {str(in_msg).rjust(23)}: {binascii.hexlify(slip_message, ' ')}", end='')
+                print(f"{in_msg} -> {out_msg} : {binascii.hexlify(slip_message, ' ')}")
                 if out_connection != None:
                     out_connection.sendall(slip_message)
-                else:
-                    print(f" -> {str(out_msg)}")      
+     
         except socket.error as error:
             print(error)
             break
 
+    print(f"\nClose {in_msg}")
+    in_connection.close()
     print(f"\nStop.\n")
 
 if __name__ == "__main__":
@@ -67,31 +67,43 @@ if __name__ == "__main__":
     communicating = True
     
     print("Python Protocol Snooper\n")
+
+    fujinet  = f"{Spoofed_AppleWin_hostname}:{Spoofed_AppleWin_port}".ljust(20)
+    applewin = f"{Real_AppleWin_hostname}:{Real_AppleWin_port}".ljust(20)
     
-    print(f"Messages received at: {Spoofed_AppleWin_hostname}:{Spoofed_AppleWin_port} (fujinet connects here, thinking it's AppleWin)")
-    print(f"will be forwarded to: {Real_AppleWin_hostname}:{Real_AppleWin_port} (This software connects to Real AppleWin)")
+    print(f"Messages received at: {fujinet} (fujinet connects here, thinking it's AppleWin)")
+    print(f"will be forwarded to: {applewin} (Real AppleWin)")
     print(f"and vice versa\n")
-    print(f"hostname: {hostname}   IP: {local_ip}\n\n")
+    print(f"hostname: {hostname}   IP: {local_ip}\n")
     
-    fujinet  = f"{Spoofed_AppleWin_hostname}:{Spoofed_AppleWin_port}"
-    applewin = f"{Real_AppleWin_hostname}:{Real_AppleWin_port}"
+
             
-    print(f"Waiting for connections here from fujinet {Spoofed_AppleWin_hostname}:{Spoofed_AppleWin_port} <Spoofed AppleWin>")
+    print(f"Waiting for fujinet to connect here: {Spoofed_AppleWin_hostname}:{Spoofed_AppleWin_port} <Spoofed AppleWin - this application>")
     # fujinet will try and connect to me
-    spoofed_applewin = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        spoofed_applewin.bind((Spoofed_AppleWin_hostname, Spoofed_AppleWin_port))
-        spoofed_applewin.listen()
-        fujinet_connection, fujinet_address = spoofed_applewin.accept()
-        print(f"\n*** Received connection from Fujinet {fujinet_address} ***\n\n")
-    except Exception as error:
-        print(error)
-        exit(-1)
+
+    while(True):
+        try:
+            spoofed_applewin = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except Exception as error:
+            print(f"1Error: '{error}'")
+            time.sleep(10)
+            continue
+        
+        try:
+            spoofed_applewin.bind((Spoofed_AppleWin_hostname, Spoofed_AppleWin_port))
+            spoofed_applewin.listen()
+            fujinet_connection, fujinet_address = spoofed_applewin.accept()
+            print(f"\n*** Received connection from Fujinet {fujinet_address} ***\n\n")
+            break
+        except Exception as error:
+            print(f"2Error: '{error}'")
+            time.sleep(10)
+            continue
         
     real_applewin = None
-    thread1_state = run
-    thread1 = threading.Thread(target = threaded_forward, args = (fujinet_connection, real_applewin, fujinet,  "LOST", thread1_state, ))
-    thread1.start()
+    #thread1_state = run
+    #thread1 = threading.Thread(target = threaded_forward, args = (fujinet_connection, real_applewin, fujinet,  "LOST", thread1_state, ))
+    #thread1.start()
 
     print(f"Attempting to connect to REAL AppleWin {Real_AppleWin_hostname}:{Real_AppleWin_port}")
     # We will connect to the real AppleWin
@@ -112,19 +124,70 @@ if __name__ == "__main__":
             time.sleep(1)
             continue
         
-    print("\n\n**** Snooping has begun! ****\n\n")
+    print("**** Snooping has begun! ****\n")
     
-    thread1_state = stop
-    
-    thread2_state = run
-    thread2 = threading.Thread(target = threaded_forward, args = (fujinet_connection, real_applewin, spoofed_applewin, applewin, thread2_state, ))
-    
-    thread3_state = run
-    thread3 = threading.Thread(target = threaded_forward, args = (real_applewin, fujinet_connection, applewin, spoofed_applewin, thread3_state, ))
-    
-    thread2.start()
-    thread3.start()
-
-    thread2.join()
-    thread3.join()
+    try:
+        thread2_state = run
+        thread2 = threading.Thread(target = threaded_forward, args = (fujinet_connection, real_applewin, "FujiNet", "AppleWin", thread2_state, ))
         
+        thread3_state = run
+        thread3 = threading.Thread(target = threaded_forward, args = (real_applewin, fujinet_connection, "AppleWin", "FujiNet", thread3_state, ))
+        
+        thread2.start()
+        thread3.start()
+
+        thread2.join()
+        thread3.join()
+    except Expection as error:
+        print(f"\nException: {error}")
+        thread2_state = stop
+        thread3_state = stop
+        print(f"\n10 second delay before stopping.")
+        time.sleep(10)
+
+
+"""
+**** Snooping has begun! ****
+
+Forward messages from FujiNet -> AppleWin
+Forward messages from AppleWin -> FujiNet
+AppleWin -> FujiNet : b'c0 24 05 01 c0'
+FujiNet -> AppleWin : b'c0 24 00 c0'
+AppleWin -> FujiNet : b'c0 25 05 02 c0'
+FujiNet -> AppleWin : b'c0 25 00 c0'
+AppleWin -> FujiNet : b'c0 26 05 03 c0'
+FujiNet -> AppleWin : b'c0 26 00 c0'
+AppleWin -> FujiNet : b'c0 27 05 04 c0'
+FujiNet -> AppleWin : b'c0 27 00 c0'
+AppleWin -> FujiNet : b'c0 28 05 05 c0'
+FujiNet -> AppleWin : b'c0 28 00 c0'
+AppleWin -> FujiNet : b'c0 29 05 06 c0'
+FujiNet -> AppleWin : b'c0 29 00 c0'
+AppleWin -> FujiNet : b'c0 2a 05 07 c0'
+FujiNet -> AppleWin : b'c0 2a 00 c0'
+AppleWin -> FujiNet : b'c0 2b 05 08 c0'
+FujiNet -> AppleWin : b'c0 2b 00 c0'
+AppleWin -> FujiNet : b'c0 2c 05 09 c0'
+FujiNet -> AppleWin : b'c0 2c ff c0'
+AppleWin -> FujiNet : b'c0 f2 00 0a 03 c0'
+AppleWin -> FujiNet : b'c0 f3 00 0b 03 c0'
+AppleWin -> FujiNet : b'c0 f4 00 0c 03 c0'
+AppleWin -> FujiNet : b'c0 f5 00 0d 03 c0'
+AppleWin -> FujiNet : b'c0 f6 00 0e 03 c0'
+AppleWin -> FujiNet : b'c0 f7 00 0f 03 c0'
+AppleWin -> FujiNet : b'c0 f8 00 10 03 c0'
+AppleWin -> FujiNet : b'c0 f9 00 11 03 c0'
+AppleWin -> FujiNet : b'c0 fa 00 12 03 c0'
+AppleWin -> FujiNet : b'c0 05 00 0a 03 c0'
+AppleWin -> FujiNet : b'c0 06 00 0b 03 c0'
+AppleWin -> FujiNet : b'c0 07 00 0c 03 c0'
+AppleWin -> FujiNet : b'c0 08 00 0d 03 c0'
+AppleWin -> FujiNet : b'c0 09 00 0e 03 c0'
+AppleWin -> FujiNet : b'c0 0a 00 0f 03 c0'
+AppleWin -> FujiNet : b'c0 0b 00 10 03 c0'
+AppleWin -> FujiNet : b'c0 0c 00 11 03 c0'
+AppleWin -> FujiNet : b'c0 0d 00 12 03 c0'
+AppleWin -> FujiNet : b'c0 18 00 0a 03 c0'
+AppleWin -> FujiNet : b'c0 19 00 0b 03 c0'
+AppleWin -> FujiNet : b'c0 1a 00 0c 03 c0'
+"""
